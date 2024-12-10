edbaa6d71e54f9dde9b6dd28955b32d0a7f7c3b4
JamesMariel <77163282+JamesMariel@users.noreply.github.com>
2024-12-10 19:43:06 +0630

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: ' January 11, 2024',
+  birthdate: ' November 11, 2024',
   name: '  Ei Yadanar Phyoe'
 };
 
